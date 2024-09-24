f21a639391d8c02fa0edb7b27effa4a61a8f388a
Chukwunenye Onyedikachi Flavian <57820624+DikachiFlavian1@users.noreply.github.com>
2024-09-24 21:34:21 +0100

    Initial commit

---
+++ src/Better.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Flavian() {
+  return (
+    <h1>
+      <b>
+        Dear Flavian, believe me when i say that i have seen the future, and it
+        is very bright, one day you will look back to this day and remember that
+        the younger you believed in this turbulent journey.
+      </b>
+    </h1>
+  );
+}
+
+export default Flavian;
---
+++ src/math.js
@@ -0,0 +1,25 @@
+const pi = 3.1415962;
+
+function doublePi() {
+  return pi * 2;
+}
+
+function triplePi() {
+  return pi * 3;
+}
+
+function Flavian() {
+  return (
+    <h1>
+      <b>
+        Dear Flavian, believe me when i say that i have seen the future, and it
+        is very bright, one day you will look back to this and remeber that the
+        younger you believed in this turbulent journey.
+      </b>
+    </h1>
+  );
+}
+
+export default pi;
+export { doublePi, triplePi };
+export { Flavian };
